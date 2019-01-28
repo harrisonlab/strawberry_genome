463121016230ff11e83bbab821e765cee5754c7f
adarmitage <Andrew.Armitage@EMR.ac.uk>
2019-01-28 09:40:52 +0000

    Updated to use new promethion data.

--- scripts/sub_racon_strawberry.sh
+++ scripts/sub_racon_strawberry.sh
@@ -4,7 +4,7 @@
 
 #$ -S /bin/bash
 #$ -cwd
-#$ -pe smp 24
+#$ -pe smp 16
 #$ -l virtual_free=15.5G
 #$ -l h=blacklace11.blacklace
 
@@ -17,11 +17,6 @@ echo "$Usage"
 # ---------------
 
 AssemblyIn=$1
-# Fastq1=$(ls /data/scratch/armita/strawberry_assembly/qc_dna/minion/F.ananassa/redgauntlet/redgauntlet_2017_reads_trim.fastq.gz)
-# Fastq2=$(ls /data/scratch/armita/strawberry_assembly/qc_dna/minion/F.ananassa/redgauntlet/redgauntlet_2018_reads_trim.fastq.gz)
-# Fastq3=$(ls /data/scratch/armita/strawberry_assembly/qc_dna/minion/F.ananassa/redgauntlet/FAH88888_trim.fastq.gz)
-# Fastq4=$(ls /data/scratch/armita/strawberry_assembly/qc_dna/minion/F.ananassa/redgauntlet/PAC21038_trim.fastq.gz)
-# Fastq5=$(ls /data/scratch/armita/strawberry_assembly/qc_dna/minion/F.ananassa/redgauntlet/PAC21093_trim.fastq.gz)
 Iterations=$2
 OutDir=$3
 
@@ -39,9 +34,9 @@ cd $WorkDir
 Assembly=$(basename $AssemblyIn)
 Assembly=$(echo $Assembly | sed 's/.utg/.fa/g')
 # Reads=appended_reads.fastq.gz
-Reads=$(ls /data2/scratch2/armita/strawberry_genome/appended_reads.fq.gz)
+# Reads=$(ls /data2/scratch2/armita/strawberry_genome/appended_reads.fq.gz)
+Reads=$(ls /data2/scratch2/armita/strawberry_genome/appended_reads_22-01-19.fq.gz)
 cp $CurDir/$AssemblyIn $Assembly
-# cat $Fastq1 $Fastq2 $Fastq3 $Fastq4 $Fastq5 > $Reads
 
 Prefix=$(echo $Assembly | cut -f1 -d '.')
 
@@ -52,11 +47,11 @@ for i in $(seq 1 $Iterations); do
   echo "Iteration - $i"
   minimap2 \
     -x map-ont \
-    -t 24 \
+    -t 16 \
     current-assembly.fa \
     $Reads \
     > racon_round_$i.reads_mapped.paf
-  racon --threads 24 $Reads racon_round_$i.reads_mapped.paf current-assembly.fa racon_round_$i.fasta
+  racon --threads 16 $Reads racon_round_$i.reads_mapped.paf current-assembly.fa racon_round_$i.fasta
   cp racon_round_$i.fasta current-assembly.fa
   cp racon_round_$i.fasta $CurDir/$OutDir/"$Prefix"_racon_round_$i.fasta
 done
